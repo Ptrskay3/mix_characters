5a65d0445dde73441da302618335b251f801e542
leeh.peter <leeh.peter@zengo.eu>
2022-05-02 16:45:12 +0200

    some progression..

--- lib/csv.ex
+++ lib/csv.ex
@@ -8,7 +8,11 @@ defmodule Csv do
   end
 
   def parse(content) do
-    [_header | rows] = content |> String.split("\n")
-    rows |> Enum.map(&String.split(&1, ";")) |> Enum.map(&Csv.Row.parse(&1))
+    [_header | rows] = content |> String.split("\n", trim: true)
+
+    rows
+    |> Enum.map(&String.split(&1, ";", trim: true))
+    |> Enum.map(&Csv.Row.parse(&1))
+    |> Enum.filter(&(map_size(&1) != 0))
   end
 end
--- lib/math.ex
+++ lib/math.ex
@@ -1,4 +1,6 @@
 defmodule Csv.Math do
+  @genders ["NA", "female", "male", "none", "hermaphrodite"]
+
   defp to_number(item) do
     case Float.parse(item) do
       {num, ""} -> num
@@ -26,5 +28,28 @@ defmodule Csv.Math do
     sum = valid_rows |> Enum.sum()
 
     sum / count
+  end
+
+  def age_distribution(state) do
+    # groups =
+    Csv.Query.get_columns(state, [:gender, :birth_year])
+    |> Enum.group_by(fn {gender, _} -> gender end)
+
+    # |> Enum.map(fn {_, v} -> v |> Enum.map(fn {_, v} -> v end) end)
+
+    # groups |> Enum.map(&Enum.filter(&1, fn a -> a != "NA" end))
+  end
+
+  def categorizer(age) when is_number(age) do
+    case age do
+      x when 0 < x and x < 21 -> :young
+      x when x >= 21 and x < 40 -> :middle
+      x when x >= 40 -> :old
+      _ -> :unknown
+    end
+  end
+
+  def categorizer(_age) do
+    :unknown
   end
 end
